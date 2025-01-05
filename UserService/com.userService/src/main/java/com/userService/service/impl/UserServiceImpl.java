b92ccb77b9c71bd5700b7f615257329378bace0b
Ayush Kumar Srivastava <ayushksrivastava29@gmail.com>
2025-01-05 21:15:29 +0530

    implemented microservices communication - Get rating and hotel details while fetching either single user or all users

--- UserService/com.userService/src/main/java/com/userService/service/impl/UserServiceImpl.java
+++ UserService/com.userService/src/main/java/com/userService/service/impl/UserServiceImpl.java
@@ -1,20 +1,31 @@
 package com.userService.service.impl;
 
+import com.userService.entities.Hotel;
+import com.userService.entities.Rating;
 import com.userService.entities.User;
 import com.userService.exceptions.ResourceNotFoundException;
 import com.userService.repositories.UserRepository;
 import com.userService.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.UUID;
+import java.util.stream.Collectors;
 
 @Service
 public class UserServiceImpl implements UserService {
 
     @Autowired
     private UserRepository userRepository;
+
+    @Autowired
+    private RestTemplate restTemplate;
+
 
     @Override
     public User saveUser(User user) {
@@ -25,14 +36,52 @@ public class UserServiceImpl implements UserService {
 
     @Override
     public List<User> getAllUsers() {
-        return userRepository.findAll();
+        List<User> users = userRepository.findAll();
+        for (User user : users) {
+            // Fetch ratings for the user
+            Rating[] ratingsArr = restTemplate.getForObject("http://localhost:8083/api/v1/ratings/getRatingByUserId/" + user.getUserId(), Rating[].class);
+            List<Rating> ratingsByUser = Arrays.stream(ratingsArr).collect(Collectors.toList());
+
+            // Fetch hotel details for each rating
+            ratingsByUser.stream().map(rating -> {
+                ResponseEntity<Hotel> hotelResponse = restTemplate.getForEntity("http://localhost:8082/api/v1/hotels/getHotel/" + rating.getHotelId(), Hotel.class);
+                Hotel hotel = hotelResponse.getBody();
+                rating.setHotel(hotel);
+                return rating;
+            }).collect(Collectors.toList());
+
+            // Set ratings to user
+            user.setRatings(ratingsByUser);
+        }
+        return users;
     }
+
 
     @Override
     public User getUser(String userId) {
-        return userRepository.findById(userId).
+        User user = userRepository.findById(userId).
                 orElseThrow(()->new
                         ResourceNotFoundException("User not found with Id: "+userId+" !!"));
+
+        //fetch rating of the given user
+        Rating[] ratingArr  = restTemplate.getForObject("http://localhost:8083/api/v1/ratings/getRatingByUserId/"+userId, Rating[].class);
+
+        List<Rating> ratingByUser;
+        ratingByUser = Arrays.stream(ratingArr).toList();
+
+        //get details of hotel also
+        ratingByUser.stream().map(rating -> {
+            ResponseEntity<Hotel> hotelList = restTemplate.getForEntity("http://localhost:8082/api/v1/hotels/getHotel/"+rating.getHotelId(), Hotel.class);
+            Hotel hotel = hotelList.getBody();
+
+            //set hotel to rating:
+            rating.setHotel(hotel);
+
+            return rating;
+            }).collect(Collectors.toList());
+
+        user.setRatings(ratingByUser);
+        return user;
     }
 
     @Override
